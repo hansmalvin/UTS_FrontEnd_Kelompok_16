13fd3c0aafbb656535ba292fe72b7ba6bab0a571
hansmalvin <hansmalvin7@gmail.com>
2024-09-25 22:29:15 +0700

    redirecting

--- pages/SignUp/script.js
+++ pages/SignUp/script.js
@@ -1,1 +1,16 @@
-// belum perlu dikasih apa apa
+$(document).ready(function() {
+    $('#signup').click(function() {
+
+        var fname = $('#fname').val();
+        var lname = $('#lname').val();
+        var email = $('#emailId').val();
+        var passwd = $('#passwd').val();
+
+        if (fname === "" || lname === "" || email === "" || passwd === "") {
+            console.log('test');
+            alert("belombisa");
+        } else {
+            window.location.href = "/index.html";
+        }
+    });
+});
