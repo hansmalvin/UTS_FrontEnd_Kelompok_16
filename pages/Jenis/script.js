aba82373ffe9415116ebf2bbad49239b7266971d
hansmalvin <hansmalvin7@gmail.com>
2024-09-25 23:31:42 +0700

    penambahan page jenis latihan

--- pages/Jenis/script.js
+++ pages/Jenis/script.js
@@ -29,16 +29,3 @@ $(document).ready(function() {
         $('.navbar').removeClass('active');
     });
 });
-
-
-const images = document.querySelectorAll('.box-container_1_2 img');
-let current = 0;
-
-function changeImage() {
-    images[current].classList.remove('active');
-    current = (current + 1) % images.length;
-    images[current].classList.add('active');
-}
-
-setInterval(changeImage, 4000); // Ganti gambar setiap 4 detik
-
--- pages/SignUp/script.js
+++ pages/SignUp/script.js
@@ -1,16 +1,7 @@
 $(document).ready(function() {
     $('#signup').click(function() {
-
-        var fname = $('#fname').val();
-        var lname = $('#lname').val();
-        var email = $('#emailId').val();
-        var passwd = $('#passwd').val();
-
-        if (fname === "" || lname === "" || email === "" || passwd === "") {
-            console.log('test');
-            alert("belombisa");
-        } else {
-            window.location.href = "/index.html";
-        }
+        
+        window.location.href = "/index.html";
+        
     });
 });
