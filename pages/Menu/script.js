2bba71b47567db54135df053cc088fa05c2dd3b7
GalangDining <dongdangding812@gmail.com>
2024-10-02 07:56:54 +0700

    Meenambahkan Navvbar  untuk setiap Pagee yang beelum ada Navbarr, dan menambahkan animasi pada Landing Page

--- pages/Menu/script.js
+++ pages/Menu/script.js
@@ -29,34 +29,3 @@ $(document).ready(function() {
         $('.navbar').removeClass('active');
     });
 });
-
-
-
-const images = document.querySelectorAll('.box-container_1_2 img');
-let current = 0;
-
-function changeImage() {
-    images[current].classList.remove('active');
-    current = (current + 1) % images.length;
-    images[current].classList.add('active');
-}
-
-setInterval(changeImage, 4000); // Ganti gambar setiap 4 detik
-
-$(document).ready(function() {
-    // Efek bounce tambahan ketika di-hover
-    $('.content p').hover(function() {
-        $(this).css('transform', 'scale(1.1)');
-    }, function() {
-        $(this).css('transform', 'scale(1)');
-    });
-
-    // Efek ketika teks di-click
-    $('.content p').on('click', function() {
-        $(this).animate({
-            fontSize: '2rem'
-        }, 300).animate({
-            fontSize: '1rem'
-        }, 300);
-    });
-});
