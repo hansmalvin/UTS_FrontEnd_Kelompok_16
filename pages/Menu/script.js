8624a50f97e3a362bf3cde6fae78348b2b8b30a7
hansmalvin <hansmalvin7@gmail.com>
2024-10-11 01:41:27 +0700

    adding search

--- pages/Menu/script.js
+++ pages/Menu/script.js
@@ -4,28 +4,33 @@ $(document).ready(function() {
         $('.navbar').toggleClass('active');
     });
 
+
     // buat alert dulu
     $('a[href="#error"]').click(function(event) {
         event.preventDefault(); 
         alert('Coming Soon !');
     });
 
-    $(window).scroll(function() {
-        $('section').each(function() {
-            let top = $(window).scrollTop();
-            let offset = $(this).offset().top - 100;
-            let height = $(this).outerHeight();
-            let id = $(this).attr('id');
-            
-            if (top >= offset && top < offset + height) {
-                $('header nav a').removeClass('active');
-                $('header nav a[href*=' + id + ']').addClass('active');
-            }
-        });
 
+    $(window).scroll(function() {
         $('header').toggleClass('scroll', $(window).scrollTop() > 100);
 
         $('#icon-utk-menu').removeClass('bx-x');
         $('.navbar').removeClass('active');
+    });
+});
+
+$(document).ready(function () {
+    $('#menuSearch').on('input', function () {
+        var searchTerm = $(this).val().toLowerCase();
+
+        $('.menu-item').each(function () {
+            var menuName = $(this).find('h2').text().toLowerCase();
+            if (menuName.includes(searchTerm)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
     });
 });
